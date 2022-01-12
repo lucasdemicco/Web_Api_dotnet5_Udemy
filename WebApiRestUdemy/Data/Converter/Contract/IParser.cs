8b8703e6443a897165d07d41ead9ead3a01c7d6f
lucasdemicco <lucasmicco@hotmail.com>
2022-01-12 12:58:02 -0300

    Mapeamento de Entidades para VO manual

---
+++ WebApiRestUdemy/Data/Converter/Contract/IParser.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace WebApiRestUdemy.Data.Converter.Contratc
+{
+    public interface IParser<D, O>
+    {
+        D Parse(O origin);
+        List<D> Parse(List<O> origin);
+    }
+}
