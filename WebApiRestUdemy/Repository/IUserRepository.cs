99408181cf01bcb20c5e144cfd46e7f790fc377f
lucasdemicco <lucasmicco@hotmail.com>
2022-01-13 15:30:06 -0300

    Repositorio e Interface do User com validação de credenciais e encript da senha

---
+++ WebApiRestUdemy/Repository/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using WebApiRestUdemy.Data.VO;
+using WebApiRestUdemy.Model;
+
+namespace WebApiRestUdemy.Repository
+{
+    public interface IUserRepository
+    {
+        User ValidateCredentials(UserVO user);
+    }
+}
