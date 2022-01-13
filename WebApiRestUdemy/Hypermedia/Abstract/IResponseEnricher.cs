b6111b89767ff3e5ac39f521dfb9a96a9ebd861a
lucasdemicco <lucasmicco@hotmail.com>
2022-01-13 10:51:46 -0300

    Parte 1 - implementação HATEOUAS

---
+++ WebApiRestUdemy/Hypermedia/Abstract/IResponseEnricher.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+
+namespace WebApiRestUdemy.Hypermedia.Abstract
+{
+    public interface IResponseEnricher
+    {
+        bool CanEnrich(ResultExecutedContext context);
+        Task Enrick(ResultExecutedContext context);
+    }
+}
---
+++ WebApiRestUdemy/Hypermedia/Constants/HttpActionVerb.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiRestUdemy.Hypermedia.Constants
+{
+    public sealed class HttpActionVerb
+    {
+        public const string GET = "GET";
+        public const string POST = "POST";
+        public const string PATCH = "PATCH";
+        public const string PUT = "PUT";
+        public const string DELETE = "DELETE";
+    }
+}
