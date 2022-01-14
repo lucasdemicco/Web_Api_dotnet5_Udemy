2dc1b8f40f0f5f7d5d3d3f7767dd159ea0cea699
lucasdemicco <lucasmicco@hotmail.com>
2022-01-14 12:36:33 -0300

    Atualizado

---
+++ UdemyApiTeste/BooksUnitTestController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiRestUdemy.Data;
+using WebApiRestUdemy.Repository;
+
+namespace UdemyApiTeste
+{
+    public class BooksUnitTestController
+    {
+        private IMapper _mapper;
+        private BookRepository _repo;
+
+        public BooksUnitTestController(IMapper mapper, BookRepository repo)
+        {
+            _mapper = mapper;
+            _repo = repo;
+        }
+
+        public static DbContextOptions<DataContext> dbContextOptions { get; }
+
+        public static string connectionString =
+            "Server=localhost;Database=rest_api_net_udemy;Uid=root;Pwd=root";
+
+        static BooksUnitTestController()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<DataContext>()
+                .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+                .Options;
+        }
+
+        public BooksUnitTestController()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new Profile());
+            });
+
+            _mapper = config.CreateMapper();
+
+            var context = new DataContext(dbContextOptions);
+
+            _repo = new BookRepository(context);
+        }
+    }
+}
---
+++ UdemyApiTeste/DbUnitTestMockIInitialize.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyApi.Domain.Model;
+using WebApiRestUdemy.Data;
+
+namespace UdemyApiTeste
+{
+    public class DbUnitTestMockIInitialize
+    {
+        public DbUnitTestMockIInitialize()
+        {
+
+        }
+
+        public void Seed(DataContext context)
+        {
+            context.Books.Add
+                (new Book { Id = 1, Author = "Mauricio de Souza", LaunchDate = DateTime.Now, Price = 19, Title = "Turma da Mônica" });
+
+            context.Books.Add
+                (new Book { Id = 2, Author = "J.R.R Tolkien", LaunchDate = DateTime.Now, Price = 99, Title = "O Senhor dos Anéis" });
+        }
+    }
+}
