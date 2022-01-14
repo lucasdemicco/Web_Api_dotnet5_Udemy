4eefd1ee24950224bf108d7f54d4f02cd7f1759b
lucasdemicco <lucasmicco@hotmail.com>
2022-01-14 10:34:12 -0300

    Add authorize em controller

--- WebApiRestUdemy/Controllers/BooksController.cs
+++ WebApiRestUdemy/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using WebApiRestUdemy.Repository;
 
 namespace WebApiRestUdemy.Controllers
 {
+    [Authorize(AuthenticationSchemes = "Bearer")]
     [ApiVersion("1")]
     [ApiController]
     [Route("/api/[controller]/v{version:apiVersion}")]
